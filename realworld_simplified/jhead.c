6500703367ccae23f1edeff301daa3d294ae5eb3
Roman Malosev <roman.malosev@uptime.eu>
2024-06-30 20:15:44 +0300

    jhead vulnerability

---
+++ realworld_simplified/jhead.c
@@ -0,0 +1,24 @@
+// https://www.cvedetails.com/cve/CVE-2022-28550/
+// repository: https://github.com/Matthias-Wandel/jhead
+// commit: 06e8d7a
+// extract of: jhead.c (function: DoCommand)
+
+#define PATH_MAX 10
+
+// idea is that when running this file then every 'a' that is passed as an argument to the progam gets replaced with the name of the program
+// following execution leads to stach smash:
+// - ./a.out a a a a a a
+
+int main(int argc, char *argv[]) {
+  int j = 0;
+  char ExecString[PATH_MAX * 3];
+
+  for (int i = 0; i < argc; i++) {
+    if (*argv[i] == 'a') {
+      int i = 0;
+      while (argv[0][i]) {
+        ExecString[j++] = argv[0][i++];
+      }
+    }
+  }
+}
