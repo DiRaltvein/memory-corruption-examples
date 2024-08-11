2aa6543b76de3e780f579f23da85407e4b302efd
Roman Malosev <roman.malosev@uptime.eu>
2024-08-11 17:57:45 +0300

    65 without repro

---
+++ realworld_simplified/xhttp.c
@@ -0,0 +1,18 @@
+// <cvedetails url>
+// repository: <repository url>
+// commit: <commit hash where vulnerability exists>
+// extract of: <path from project root to file with vulnerability> (function: <function name with vulnerability>)
+
+#include <ctime>    // time
+#include <stdio.h>  // printf
+#include <stdlib.h> // rand
+
+int getAnInt(int max) {
+  return rand() % (max + 1);
+}
+
+int main(int argc, char *argv[]) {
+  srand(time(0));
+  // Repro
+  printf("%i, %s, %c, %.2f\n", 5, "string", 'R', 5.235);
+}
