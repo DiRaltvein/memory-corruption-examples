f2491b732399f0a0e30544f2056352d5f1e72f73
Roman Malosev <roman.malosev@uptime.eu>
2024-08-21 12:12:38 +0300

    72

---
+++ realworld_simplified/cups.c
@@ -0,0 +1,15 @@
+// https://www.cvedetails.com/cve/CVE-2023-4504/
+// repository: https://github.com/OpenPrinting/cups
+// commit: 9d57699
+// extract of: cups/raster-interpret.c (function: scan_ps)
+
+int main(int argc, char *argv[]) {
+  char *cur = "(Hello\\";
+
+  for (cur++; *cur; cur++) {
+    if (*cur == '\\') {
+      cur++; // Problem: after addition if cur pointer was already pointing to the last character in a string it will point to the null byte
+      // it leads to reading outside of the string because in the end of for cycle cur pointer is once again incremented by the for loop
+    }
+  }
+}
