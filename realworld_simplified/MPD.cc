85b6709670cd4e7f73711e4344195f327157bbe6
Roman Amjaga <roman.amjaga@uptime.eu>
2025-01-29 09:12:13 +0200

    added file length to all tested files

--- realworld_simplified/MPD.cc
+++ realworld_simplified/MPD.cc
@@ -5,8 +5,19 @@
 
 #include <stdio.h>
 
-int main() {
-  char buffer[16];
-  size_t length = snprintf(buffer, sizeof(buffer), "%s", "Sunsets are gold");
-  *(buffer + length) = ':'; // Problem: write out of bound by one byte
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    return 1;
+  }
+
+  char buffer[60];
+  // the problem is  that snprintf returns 'the number of characters that would have been written if n had been sufficiently large'
+  // as per https://cplusplus.com/reference/cstdio/snprintf/
+  // So if string to write into buffer is longer than 30 (50 - 20) bytes then nothing is written into buffer and the length of the string is returned
+	size_t length = snprintf(buffer, sizeof(buffer) - 20, "Your privided string: %s", argv[1]);
+	char *p = buffer + length;
+	*p++ = ':'; // Problem: write out of bound by one byte
+	*p++ = ' '; // Problem: write out of bound by one byte
+
+  printf("%s\n", buffer);
 }
