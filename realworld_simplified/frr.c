f9b7caf35af77bea3bcc19c24e5650ba6cc0e63b
Roman Amjaga <roman.amjaga@uptime.eu>
2025-02-28 09:33:48 +0200

    refactor

--- realworld_simplified/frr.c
+++ realworld_simplified/frr.c
@@ -3,18 +3,30 @@
 // commit: c2b0d6a
 // extract of: bgpd/bgp_open.c (function: bgp_capability_software_version)
 
-#include <string.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    return 0;
+#define BGP_MAX_SOFT_VERSION 64
+
+int main() {
+  char version[] = "\x60Version string that is a little bit longer then program expects but nevertheless the whole string length is greater then the length specified in the beggining of the string.";
+  char *versionp = (char *)version;
+  size_t verStringLength = strlen(version);
+  uint8_t len = *versionp++;
+
+  if (!len) {
+    printf("Could not read version from argument string\n");
+    return 1;
   }
 
-  char str[65];
-  size_t len = strlen(argv[1]);
-  memcpy(str, argv[1], len); // Problem: terminal parametr length can be greater than 65 characters that would result in a str buffer overflow
+  if (verStringLength - 1 < len) {
+    printf("malformed version string\n");
+    return 1;
+  }
+
+  char str[BGP_MAX_SOFT_VERSION + 1];
+  memcpy(str, versionp, len); // Problem: len can be greater than str size resulting in a buffer overflow
   str[len] = '\0';
-  printf("%s\n", str);
+  printf("Extracted version: %s\n", str);
 }
--- realworld_simplified/jhead_1.c
+++ realworld_simplified/jhead_1.c
@@ -3,35 +3,52 @@
 // commit: e64e904
 // extract of: jpgfile.c (function: process_COM)
 
-#include <stdio.h> // printf
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define MAX_COMMENT_SIZE 32
+typedef unsigned char uchar;
+#define MAX_COMMENT_SIZE 16000
+#define EXAMPLE_LENGTH 17
 
-int main(int argc, char *argv[]) {
-  if (argc < 2)
-    return 1;
-  char *Data = argv[1]; // take a string from command prompt
-  int length = strlen(Data);
-
+static void process_COM(uchar *Data, int length) {
+  int ch;
   char Comment[MAX_COMMENT_SIZE + 1];
   int nch;
+  int a;
 
   nch = 0;
 
   if (length > MAX_COMMENT_SIZE)
-    length = MAX_COMMENT_SIZE;
+    length = MAX_COMMENT_SIZE; // Truncate if it won't fit in our structure.
 
-  for (int a = 0; a < length; a++) {
-    int ch = Data[a];
+  for (a = 2; a < length; a++) {
+    ch = Data[a];
 
-    if (ch == 'a' && Data[a + 1] == 'a') // Problem: in case a is the last character in a string then Data[a+1] will result in a one byte read past end of the string
-      continue;
+    if (ch == '\r' && Data[a + 1] == '\n') // Problem: Data[a + 1] results in read out of bound by 1 byte in case \r is the last character in a string
+      continue;                            // Remove cr followed by lf.
 
-    Comment[nch++] = (char)ch;
+    if (ch >= 32 || ch == '\n' || ch == '\t') {
+      Comment[nch++] = (char)ch;
+    } else {
+      Comment[nch++] = '?';
+    }
   }
 
-  Comment[nch] = '\0';
+  Comment[nch] = '\0'; // Null terminate
 
   printf("COM marker comment: %s\n", Comment);
+}
+
+int main() {
+  uchar *data = calloc(EXAMPLE_LENGTH, sizeof(uchar));
+  if (data == NULL) {
+    printf("Out of memory\n");
+    return 1;
+  }
+  // memcpy such that data buffer is not null terminated. Is needed for memory sanitizers to detect 1 read out of bound.
+  // Otherwise, if string is null terminated zero byte is read and no error is outputted
+  memcpy(data, "Some random data\r", EXAMPLE_LENGTH);
+  process_COM(data, EXAMPLE_LENGTH);
+  free(data);
 }
