ceca3b8553a1abcb6cc2fa3714475f5b543e5e3c
Roman Amjaga <roman.amjaga@uptime.eu>
2025-03-02 13:53:36 +0200

    refactor

--- realworld_simplified/MicroHttpServer.c
+++ realworld_simplified/MicroHttpServer.c
@@ -1,26 +1,110 @@
-// https://www.cvedetails.com/cve/CVE-2023-50965/
+// https://www.cvedetails.com/cve/CVE-2023-51771/
 // repository: https://github.com/starnight/MicroHttpServer
-// commit: 4398570
-// extract of: c-version/lib/middleware.c (function: _ReadStaticFiles)
+// commit: a8ab029
+// extract of: c-version/lib/server.c (function: _ParseHeader)
 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define STATIC_FILE_FOLDER "static/"
+#include <sys/socket.h>
+#include <unistd.h>
 
-// the idea is that this is a function that returns files stored inside static folder of a web server
-// when request comes extract file path and return the file. Default static directory is 'static/' and to that add
-// the file path/name that is in the request
-int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    return 0;
+void _ParseHeader(int *clisock, char *req_buf) {
+  int n;
+  int i = 0;
+  char *p = req_buf;
+
+  n = recv(*clisock, p, 3, 0);
+  if (n == 3) {
+    for (i = 3; n > 0; i++) {
+      n = recv(*clisock, p + i, 1, 0);
+      if (p[i] == ' ') {
+        p[i] = '_';
+        break;
+      }
+    }
+
+    /* Parse URI. */
+    if (n > 0)
+      i += 1;
+
+    for (; n > 0; i++) {
+      n = recv(*clisock, p + i, 1, 0); // Problem: code is taking data from client socket and copying to over to req_buf 1 byte at a time while
+      // waiting for an empty space. This is a potential req_buf overflow, if url is longer then the req_buf size
+      if (p[i] == ' ') {
+        p[i] = '\0';
+        break;
+      }
+    }
+  }
+}
+
+char *getResponse() {
+  char *output = calloc(3000, sizeof(char));
+  char *content = "<h1>Hello</h1>";
+  char *httpVersion = "HTTP/1.1 200 OK\r\n";
+  char *contentLengthTitle = "Content-Length: ";
+
+  char contentLengthSize[10] = {0};
+  sprintf((char *)contentLengthSize, "%d", (int)strlen(content));
+
+  char *newLine = "\r\n";
+  char contentLength[40] = {0};
+  strncat(contentLength, contentLengthTitle, strlen(contentLengthTitle));
+  strncat(contentLength, (char *)contentLengthSize, strlen((char *)contentLengthSize));
+  strncat(contentLength, newLine, strlen(newLine));
+
+  char *server = "Server: custom\r\n";
+  char *contentType = "Content-Type: text/html; charset=UTF-8";
+  strncat(output, httpVersion, strlen(httpVersion));
+  strncat(output, contentLength, strlen(contentLength));
+  strncat(output, server, strlen(server));
+  strncat(output, contentType, strlen(contentType));
+  strncat(output, newLine, strlen(newLine));
+  strncat(output, newLine, strlen(newLine));
+  strncat(output, content, strlen(content));
+  strncat(output, newLine, strlen(newLine));
+  strncat(output, newLine, strlen(newLine));
+  return output;
+}
+
+int main() {
+  struct sockaddr_in srv_addr;
+  char req_buf[20];
+  int sock;
+  int clisock;
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sock <= 0)
+    return -1;
+
+  memset(&srv_addr, 0, sizeof(struct sockaddr_in));
+  srv_addr.sin_family = AF_INET;
+  srv_addr.sin_port = htons(8080);
+  srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sock, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) == -1) {
+    exit(1);
+  }
+  listen(sock, 0);
+
+  while (1) {
+    struct sockaddr_in cli_addr;
+    int client_name_len = sizeof(struct sockaddr_in);
+    if ((clisock = accept(sock, (struct sockaddr *)&cli_addr, (socklen_t *)&client_name_len)) < 0) {
+      return -1;
+    }
+    _ParseHeader(&clisock, req_buf);
+    printf("%s\n", req_buf);
+    char *response = getResponse();
+    write(clisock, response, strlen(response));
+    close(clisock);
+    free(response);
   }
 
-  char path[128] = {STATIC_FILE_FOLDER};
-  char *uri = argv[1]; // mock file name or path to retrieve
+  //
 
-  memcpy(path + strlen(STATIC_FILE_FOLDER), uri, strlen(uri)); // Problem: lenght of uri is not checked and it can be larger then the buffer of 128 bytes.
-
-  // following logic would be to open the file with path and return in if exists
-  // fp = fopen(path, "r");
-  // if(fp != NULL) {
-  // ...
+  shutdown(sock, SHUT_RDWR);
+  close(sock);
 }
--- realworld_simplified/moonlight-common-c.c
+++ realworld_simplified/moonlight-common-c.c
@@ -6,16 +6,51 @@
 // same vulnerability as https://www.cvedetails.com/cve/CVE-2023-26767/
 // same vulnerability as https://www.cvedetails.com/cve/CVE-2023-26769/
 
-#include <string.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int extractVersionQuadFromString(const char *string, int *quad) {
+  char versionString[128];
+  char *nextDot;
+  char *nextNumber;
+  int i;
+
+  strcpy(versionString, string); // Problem: passed version string may be greater than 128 characters leading to versionString buffer overflow
+  nextNumber = versionString;
+
+  for (i = 0; i < 4; i++) {
+    if (i == 3) {
+      nextDot = strchr(nextNumber, '\0');
+    } else {
+      nextDot = strchr(nextNumber, '.');
+    }
+    if (nextDot == NULL) {
+      return -1;
+    }
+
+    // Cut the string off at the next dot
+    *nextDot = '\0';
+
+    quad[i] = atoi(nextNumber);
+
+    // Move on to the next segment
+    nextNumber = nextDot + 1;
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    return 0;
+    printf("Usage: %s <version string in format major.minor.patch.build (example 5.12.652.1)", argv[0]);
+    return 1;
   }
 
-  char versionString[128];
-  strcpy(versionString, argv[1]); // Problem: terminal parametr length can be greater than 128 characters that would result in a versionString buffer overflow
-  printf("%s\n", versionString);
+  int quad[4] = {0};
+  if (extractVersionQuadFromString(argv[1], quad) == 0) {
+    printf("Successfully parsed version string. Major: %d, minor: %d, patch: %d, build: %d.\n", quad[0], quad[1], quad[2], quad[3]);
+  } else {
+    printf("Could not extract versions from passed string\n");
+  }
 }
