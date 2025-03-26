e8d73040d6fb95bafa00264226ab62ea51eac7f7
Roman Amjaga <roman.amjaga@uptime.eu>
2025-03-26 10:07:41 +0200

    converted categories into CWE numbers

---
+++ scripts/generateCategories.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import cves from '../table.json' assert { type: 'json' };
+import { parse } from 'node-html-parser';
+
+const base = 'https://www.cvedetails.com/cve/';
+
+// fetch a html page and parse it using node-html-parser. It is extracted into a separate method because cvedetails uses a cloudflare and it often responds with 429
+async function getAPage(url) {
+  const page = await fetch(url);
+  if (page.status === 429) {
+    console.log('retry...');
+    await new Promise((res) => setTimeout(res, 20_000));
+    return getAPage(url);
+  }
+  return parse(await page.text());
+}
+
+async function main() {
+  for (const cve of cves) {
+    if (!cve.id) continue;
+    const page = await getAPage(`${base}${cve.CVE}`);
+    cve['vuln. type'] = Array.from(
+      page.querySelectorAll('a[title$="- CWE definition"]')
+    )
+      .filter((el) => el._rawAttrs.title)
+      .map((el) => el._rawAttrs.title.replace(' - CWE definition', '').trim())
+      .join('/');
+    console.log(cve.CVE, ' - ', cve['vuln. type']);
+  }
+
+  fs.writeFileSync('table.json', JSON.stringify(cves, null, 2));
+}
+
+main();
--- scripts/tableStats.js
+++ scripts/tableStats.js
@@ -1,14 +1,27 @@
 // https://tableconvert.com/markdown-to-csv
 import cves from '../table.json' assert { type: 'json' };
 
-import { CategoryScale, Chart, LinearScale, LineController, LineElement, PointElement, BarController, BarElement, LogarithmicScale } from 'chart.js';
+import {
+  CategoryScale,
+  Chart,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  BarController,
+  BarElement,
+  LogarithmicScale,
+} from 'chart.js';
 import { Canvas } from 'skia-canvas';
 import fsp from 'node:fs/promises';
 import lodash from 'lodash';
 
 const calculateForTestSuit = true;
 
-const cvesToAnalyze = cves.filter(cve => (calculateForTestSuit && !cve.id) || (!calculateForTestSuit && cve.id));
+const cvesToAnalyze = cves.filter(
+  (cve) =>
+    (calculateForTestSuit && !cve.id) || (!calculateForTestSuit && cve.id)
+);
 const analyzersToUse = [
   'clang',
   'cppcheck',
@@ -16,12 +29,17 @@ const analyzersToUse = [
   'infer',
   'gcc',
   ...(calculateForTestSuit ? ['symbiotic'] : []),
-]
+];
 
 const getCveNumber = (cve) => {
   if (cve.id) return cve.CVE;
   return cves[cves.indexOf(cve) - 1].CVE;
-}
+};
+
+const getCategories = (cve) => {
+  if (cve.id) return cve['vuln. type'].split('/');
+  return cves[cves.indexOf(cve) - 1]['vuln. type'].split('/');
+};
 
 const allFileLengths = [];
 const allVulnFunctionsLengths = [];
@@ -35,41 +53,96 @@ for (const cve of cvesToAnalyze) {
 console.log('Vulnerable file length:');
 console.log('max: ', Math.max(...allFileLengths));
 console.log('min: ', Math.min(...allFileLengths));
-console.log('avg: ', Math.round(allFileLengths.reduce((sum, l) => sum + l, 0) / allFileLengths.length));
+console.log(
+  'avg: ',
+  Math.round(
+    allFileLengths.reduce((sum, l) => sum + l, 0) / allFileLengths.length
+  )
+);
 
 console.log('\nVulnerable function length:');
 console.log('max: ', Math.max(...allVulnFunctionsLengths));
 console.log('min: ', Math.min(...allVulnFunctionsLengths));
-console.log('avg: ', Math.round(allVulnFunctionsLengths.reduce((sum, l) => sum + l, 0) / allVulnFunctionsLengths.length));
+console.log(
+  'avg: ',
+  Math.round(
+    allVulnFunctionsLengths.reduce((sum, l) => sum + l, 0) /
+      allVulnFunctionsLengths.length
+  )
+);
+
+console.log(
+  '\ncategories: ',
+  cvesToAnalyze.reduce((acc, cve) => {
+    const categories = getCategories(cve);
+    for (const cat of categories) {
+      acc[cat] = (acc[cat] ?? 0) + 1;
+    }
+    return acc;
+  }, {})
+);
 
 for (const analyzer of analyzersToUse) {
-  const error = cvesToAnalyze.reduce((acc, cve) => acc += cve[analyzer] === '✖' ? 1 : 0, 0);
-  const failed = cvesToAnalyze.reduce((acc, cve) => acc +=  (cve[analyzer].includes('✖/✖') ? 1 : 0), 0);
-  const success = cvesToAnalyze.reduce((acc, cve) => acc += (cve[analyzer].includes('✓') ? 1 : 0), 0);
-  const successIn5Rows = cvesToAnalyze.reduce((acc, cve) => acc += (cve[analyzer].includes('✓/✓') ? 1 : 0), 0);
+  const error = cvesToAnalyze.reduce(
+    (acc, cve) => (acc += cve[analyzer] === '✖' ? 1 : 0),
+    0
+  );
+  const failed = cvesToAnalyze.reduce(
+    (acc, cve) => (acc += cve[analyzer].includes('✖/✖') ? 1 : 0),
+    0
+  );
+  const success = cvesToAnalyze.reduce(
+    (acc, cve) => (acc += cve[analyzer].includes('✓') ? 1 : 0),
+    0
+  );
+  const successIn5Rows = cvesToAnalyze.reduce(
+    (acc, cve) => (acc += cve[analyzer].includes('✓/✓') ? 1 : 0),
+    0
+  );
   const numberOfNotes = {
     notes: 0,
     warnings: 0,
-    errors: 0
+    errors: 0,
   };
   for (const cve of cvesToAnalyze) {
     if (cve[analyzer].length < 5) continue;
     const notes = cve[analyzer].split('(')[0];
     const notesSplit = notes.split('/');
-    if (notesSplit.length !== 3) console.error('cve', cve, analyzer, 'could not parse stats');
+    if (notesSplit.length !== 3)
+      console.error('cve', cve, analyzer, 'could not parse stats');
     numberOfNotes.notes += parseInt(notesSplit[2]);
     numberOfNotes.warnings += parseInt(notesSplit[1]);
     numberOfNotes.errors += parseInt(notesSplit[0]);
   }
-  console.log(`\n${analyzer} statistics:`)
-  console.log('errors: ', error, ' failed: ', failed, ' success: ', success, ' success in first 5 rows: ', successIn5Rows);
-  console.log('total num of notes: ', numberOfNotes.errors + numberOfNotes.warnings + numberOfNotes.notes, ' errors: ', numberOfNotes.errors, 'warnings: ', numberOfNotes.warnings, ' notes: ', numberOfNotes.notes);
+  console.log(`\n${analyzer} statistics:`);
+  console.log(
+    'errors: ',
+    error,
+    ' failed: ',
+    failed,
+    ' success: ',
+    success,
+    ' success in first 5 rows: ',
+    successIn5Rows
+  );
+  console.log(
+    'total num of notes: ',
+    numberOfNotes.errors + numberOfNotes.warnings + numberOfNotes.notes,
+    ' errors: ',
+    numberOfNotes.errors,
+    'warnings: ',
+    numberOfNotes.warnings,
+    ' notes: ',
+    numberOfNotes.notes
+  );
   // failed is FN as tool outputted that file has no error while error was there
   // success is TP because tool outputted that file has error while error was there
-  console.log('True Positive Rate (TPR): ', (success / (success + failed)).toFixed(4));
+  console.log(
+    'True Positive Rate (TPR): ',
+    (success / (success + failed)).toFixed(4)
+  );
 }
 // Simple console log stats
-
 
 Chart.register([
   BarController,
@@ -79,73 +152,79 @@ Chart.register([
   LineController,
   LineElement,
   LinearScale,
-  PointElement
+  PointElement,
 ]);
 
 //
 const canvas = new Canvas(1920, 1200);
-const fileLenChart = new Chart(
-  canvas,
-  {
-    type: 'bar',
-    data: {
-      labels: cvesToAnalyze.map(getCveNumber),
-      datasets: [{
-        data: cvesToAnalyze.map(c => parseInt(c['file len'])),
+const fileLenChart = new Chart(canvas, {
+  type: 'bar',
+  data: {
+    labels: cvesToAnalyze.map(getCveNumber),
+    datasets: [
+      {
+        data: cvesToAnalyze.map((c) => parseInt(c['file len'])),
         borderWidth: 1,
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          type: 'logarithmic',
-          title: {
-            display: true,
-            text: 'File length'
-          }
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        type: 'logarithmic',
+        title: {
+          display: true,
+          text: 'File length',
         },
-      }
-    }
-  }
-);
+      },
+    },
+  },
+});
 
-const fileLenPngBuffer = await canvas.toBuffer('png', {matte: 'white'});
-await fsp.writeFile(`charts/fileLenChart${calculateForTestSuit ? '-suit' : ''}.png`, fileLenPngBuffer);
+const fileLenPngBuffer = await canvas.toBuffer('png', { matte: 'white' });
+await fsp.writeFile(
+  `charts/fileLenChart${calculateForTestSuit ? '-suit' : ''}.png`,
+  fileLenPngBuffer
+);
 fileLenChart.destroy();
 //
 
 //
-const funcChart = new Chart(
-  canvas,
-  {
-    type: 'bar',
-    data: {
-      labels: cvesToAnalyze.map(getCveNumber),
-      datasets: [{
-        data: cvesToAnalyze.map(c => parseInt(c['func len'])),
+const funcChart = new Chart(canvas, {
+  type: 'bar',
+  data: {
+    labels: cvesToAnalyze.map(getCveNumber),
+    datasets: [
+      {
+        data: cvesToAnalyze.map((c) => parseInt(c['func len'])),
         borderWidth: 1,
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          type: 'logarithmic',
-          title: {
-            display: true,
-            text: 'Function length'
-          }
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        type: 'logarithmic',
+        title: {
+          display: true,
+          text: 'Function length',
         },
-      }
-    }
-  }
-);
+      },
+    },
+  },
+});
 
-const funcPngBuffer = await canvas.toBuffer('png', {matte: 'white'});
-await fsp.writeFile(`charts/funcChart${calculateForTestSuit ? '-suit' : ''}.png`, funcPngBuffer);
+const funcPngBuffer = await canvas.toBuffer('png', { matte: 'white' });
+await fsp.writeFile(
+  `charts/funcChart${calculateForTestSuit ? '-suit' : ''}.png`,
+  funcPngBuffer
+);
 funcChart.destroy();
 //
 
-const sortedFuncLengths = lodash.sortBy(cvesToAnalyze, (c => parseInt(c['func len'])));
+const sortedFuncLengths = lodash.sortBy(cvesToAnalyze, (c) =>
+  parseInt(c['func len'])
+);
 const buckets = lodash.chunk(sortedFuncLengths, 5);
 
 const getDetectionRateInBucket = (bucket, analyzers) => {
@@ -157,8 +236,10 @@ const getDetectionRateInBucket = (bucket, analyzers) => {
     }
   }
 
-  return (overallDetectedVulnerabilities / (bucket.length * analyzers.length)) * 100;
-}
+  return (
+    (overallDetectedVulnerabilities / (bucket.length * analyzers.length)) * 100
+  );
+};
 
 // generate a graph that would show how ability to detect scales with function size
 // Line graph where x would be function length where vulnerabilit happened (by buckets) found and y would be % of vulnerabilities found
@@ -166,73 +247,89 @@ const getDetectionRateInBucket = (bucket, analyzers) => {
 // % of vulnerabilities detected is just a sum of all detected cases divided by total cases and mltiplied by 100.
 //  For example, if one test case was correctly identified by 2 static code analyzers out of 5 and another test case was correctly identified by 1 static code analzer then the detection rate is
 // (2 + 1) / (2 * 5) * 100    <- total number of correctly identified cases divided by total number of identifications (in total 10 as 5 static code analyzers and 2 different test cases) multiplied by 100 to get percentage in range [0, 100]
-const detectedVulnsRatioByFuncLen = new Chart(
-  canvas,
-  {
+const detectedVulnsRatioByFuncLen = new Chart(canvas, {
+  type: 'line',
+  data: {
+    yLabels: '%',
+    labels: buckets.map(
+      (bucket) =>
+        `${bucket[0]['func len']} - ${bucket[bucket.length - 1]['func len']}`
+    ),
+    datasets: [
+      {
+        data: buckets.map((b) => getDetectionRateInBucket(b, analyzersToUse)),
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: '% of vulnerabilities detected',
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: 'Length of function containing vulnerability',
+        },
+      },
+    },
+  },
+});
+
+const detectedVulnsPngBuffer = await canvas.toBuffer('png', { matte: 'white' });
+await fsp.writeFile(
+  `charts/detectedVulnsRatioByFuncLen${
+    calculateForTestSuit ? '-suit' : ''
+  }.png`,
+  detectedVulnsPngBuffer
+);
+detectedVulnsRatioByFuncLen.destroy();
+//
+
+if (!calculateForTestSuit) {
+  const detectedVulnsRatioByFuncLenIkos = new Chart(canvas, {
     type: 'line',
     data: {
       yLabels: '%',
-      labels: buckets.map(bucket => `${bucket[0]['func len']} - ${bucket[bucket.length - 1]['func len']}`),
-      datasets: [{
-        data: buckets.map(b => getDetectionRateInBucket(b, analyzersToUse))
-      }]
+      labels: buckets.map(
+        (bucket) =>
+          `${bucket[0]['func len']} - ${bucket[bucket.length - 1]['func len']}`
+      ),
+      datasets: [
+        {
+          data: buckets.map((b) => getDetectionRateInBucket(b, ['ikos'])),
+        },
+      ],
     },
     options: {
       scales: {
         y: {
           title: {
             display: true,
-            text: '% of vulnerabilities detected'
-          }
+            text: '% of vulnerabilities detected',
+          },
         },
         x: {
           title: {
             display: true,
-            text: 'Length of function containing vulnerability'
-          }
-        }
-      }
-    }
-  }
-);
-
-const detectedVulnsPngBuffer = await canvas.toBuffer('png', {matte: 'white'});
-await fsp.writeFile(`charts/detectedVulnsRatioByFuncLen${calculateForTestSuit ? '-suit' : ''}.png`, detectedVulnsPngBuffer);
-detectedVulnsRatioByFuncLen.destroy();
-//
-
-if (!calculateForTestSuit) {
-  const detectedVulnsRatioByFuncLenIkos = new Chart(
-    canvas,
-    {
-      type: 'line',
-      data: {
-        yLabels: '%',
-        labels: buckets.map(bucket => `${bucket[0]['func len']} - ${bucket[bucket.length - 1]['func len']}`),
-        datasets: [{
-          data: buckets.map(b => getDetectionRateInBucket(b, ['ikos']))
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            title: {
-              display: true,
-              text: '% of vulnerabilities detected'
-            }
+            text: 'Length of function containing vulnerability',
           },
-          x: {
-            title: {
-              display: true,
-              text: 'Length of function containing vulnerability'
-            }
-          }
-        }
-      }
-    }
-  );
+        },
+      },
+    },
+  });
 
-  const detectedVulnsIkosPngBuffer = await canvas.toBuffer('png', {matte: 'white'});
-  await fsp.writeFile(`charts/detectedVulnsRatioByFuncLenIkos${calculateForTestSuit ? '-suit' : ''}.png`, detectedVulnsIkosPngBuffer);
+  const detectedVulnsIkosPngBuffer = await canvas.toBuffer('png', {
+    matte: 'white',
+  });
+  await fsp.writeFile(
+    `charts/detectedVulnsRatioByFuncLenIkos${
+      calculateForTestSuit ? '-suit' : ''
+    }.png`,
+    detectedVulnsIkosPngBuffer
+  );
   detectedVulnsRatioByFuncLenIkos.destroy();
 }
